c1f4165d70dd521939a43bdd78ce8e91810a9382
Maribel <marleyva233@gmail.com>
2018-07-15 16:20:45 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+//the function will be triggered when clicking a button
+function submitMyComment()
+//we want to submit the information from the 
+//input and textarea field so they need a class or id
