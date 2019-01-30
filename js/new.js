d8b8b0cbb96886eb1ccbb79e9f56c0dbd12e53ee
Maribel <marleyva233@gmail.com>
2019-01-30 12:03:11 -0800

    added fonts and new styling

--- js/new.js
+++ js/new.js
@@ -4,18 +4,21 @@ function submitMyComment() {
 	var myComment = document.getElementById("myComment");
 	var parentDiv = document.getElementById("divParent");
 	var date = new Date();
-	if (userName.value == "" || myComment.value == "") {
+	if (userName == "" || myComment == "") {
 		alert("Please enter your username and/or comment!");
 		return false;
 	}
 	var newComment = document.createElement("div");
 		newComment.id = "comment" + counter;
-		newComment.innerHTML = "<h2>" + userName.value + "</h2>";
+		newComment.classList.add("col-10", "mx-auto");
+		newComment.innerHTML = "<h4>" + userName.value + "</h2>";
 		newComment.innerHTML += "<p>" + myComment.value + "</p>";
 		newComment.innerHTML += "<span class='smalldate'>" + date + "</span>";
-		newComment.innerHTML += "<button class='btn btn-info' onclick='deletingComment(" + counter + ");'> Delete Comment </button>";
+		newComment.innerHTML += "<button class='deleteBtn' onclick='deletingComment(" + counter + ");'> Delete Comment </button>";
 	parentDiv.appendChild(newComment);
 	counter++;
+	userName.value="";
+	myComment.value="";
 }
 function deletingComment(num) {
 	var commentsArea = document.getElementById("divParent");
